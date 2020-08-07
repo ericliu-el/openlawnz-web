175eef373805d053e2a958aba3ca6e37b738ec19
Eric Liu <eric@ericliu.page>
2020-08-07 21:15:39 +1200

    add h1-h5 styles

--- src/components/Hero/index.jsx
+++ src/components/Hero/index.jsx
@@ -6,7 +6,7 @@ import styles from "./hero.module.scss"
 const Hero = () => (
   <div className={styles.hero}>
     <HeroBg />
-    <p>A new, free legal research platform for New Zealand.</p>
+    <h1>A new, free legal research platform for New Zealand.</h1>
   </div>
 )
 
